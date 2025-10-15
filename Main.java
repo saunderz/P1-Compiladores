c7aead8cc2db143e5fc1325079884104874b0469
unknown <saunders.luan@gmail.com>
2025-10-15 18:06:51 -0300

    chore(main): Parte 8 - integra Parser com Interpretador e executa programa

--- Main.java
+++ Main.java
@@ -1,11 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        String input =
-            "let a = 42 + 5 - 8;\n" +
-            "let b = 56 + 8;\n" +
-            "print a + b + 6;";
+        
+        /* 
+        String input = """
+                push 10
+                push 20
+                add
+                pop a
+                push 45
+                push a
+                sub
+                print    
+                """;
+        */
 
-        Parser p = new Parser(input.getBytes());
+        String input = """
+            let a = 42 + 2;
+            let b = 15 + 3;
+            print a + b;        
+                """;
+        
+        Parser p = new Parser (input.getBytes());
         p.parse();
+
+        Interpretador i = new Interpretador (p.output());
+        i.run();
     }
 }

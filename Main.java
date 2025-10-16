a831587be57d16de820ac89ae35beef2fc2b1e87
saunderz <67610539+saunderz@users.noreply.github.com>
2025-10-15 22:46:35 -0300

    chore(main): Parte 8 — exemplo let/print; pipeline Parser→Interpretador

--- Main.java
+++ Main.java
@@ -1,19 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        
-        /* 
-        String input = """
-                push 10
-                push 20
-                add
-                pop a
-                push 45
-                push a
-                sub
-                print    
-                """;
-        */
-
         String input = """
             let a = 42 + 2;
             let b = 15 + 3;
